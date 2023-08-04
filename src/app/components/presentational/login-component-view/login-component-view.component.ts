32bdb79d8c778c8405c1165706fdeec9adf76b10
Vlad Miron <Vlad.Miron@msg.group>
2023-08-04 16:21:30 +0300

    solves braces, injection and emitter types

---
+++ src/app/components/presentational/login-component-view/login-component-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-component-view',
+  templateUrl: './login-component-view.component.html',
+  styleUrls: ['./login-component-view.component.scss']
+})
+export class LoginComponentViewComponent {
+
+}
--- src/app/components/presentational/products-details-view/products-details-view.component.ts
+++ src/app/components/presentational/products-details-view/products-details-view.component.ts
@@ -11,7 +11,7 @@ export class ProductsDetailsViewComponent {
   @Input() shoppingCartImageUrl: String =
     'https://www.bing.com/images/search?view=detailV2&ccid=hd6cAKPA&id=A044F2E65A996F214578F90EC88C8F48BD14A299&thid=OIP.hd6cAKPAERPHDbVUFWRbyAHaHa&mediaurl=https%3A%2F%2Fimages.designtrends.com%2Fwp-content%2Fuploads%2F2015%2F11%2F27111316%2FShopping-Cart-Icons35.png&cdnurl=https%3A%2F%2Fth.bing.com%2Fth%2Fid%2FR.85de9c00a3c01113c70db55415645bc8%3Frik%3DmaIUvUiPjMgO%252bQ%26pid%3DImgRaw%26r%3D0&exph=1200&expw=1200&q=shopping+cart+png+icon&simid=608011243316977000&form=IRPRST&ck=19702DDA66BB9A3312DB37D868B833D5&selectedindex=1&ajaxhist=0&ajaxserp=0&vt=0&sim=11';
   @Output() addToShoppingCart: EventEmitter<OrderProduct> = new EventEmitter();
-  @Output() deleteProduct: EventEmitter<string> = new EventEmitter();
-  @Output() navigateToProductList: EventEmitter<string> = new EventEmitter();
+  @Output() deleteProduct: EventEmitter<void> = new EventEmitter();
+  @Output() navigateToProductList: EventEmitter<void> = new EventEmitter();
   @Output() navigateToProductEdit: EventEmitter<string> = new EventEmitter();
 }
